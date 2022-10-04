4a89e132419d7647ddbea4a51dbbc30c16a52d57
Alexis Herrera <alexisherrera@Alexiss-MacBook-Pro.local>
2022-10-04 15:37:25 -0400

    i give up on this

--- script.js
+++ script.js
@@ -1,128 +1,93 @@
-let squares = document.querySelectorAll(".square")
-let currentPlayer = null;
-let reset = document.querySelector(".reset")
-let players = ['p1', 'p2']
-let turn = document.querySelector('h3')
-let tracker;
-let playTracker = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""]
-]
+const statusDisplay = document.querySelector(".game--status");
 
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
 
-currentPlayer = players[(Math.floor(Math.random() * players.length))]
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
+statusDisplay.innerHTML = currentPlayerTurn();
 
-turn.innerText = `Player: ${currentPlayer}`
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+  gameState[clickedCellIndex] = currentPlayer;
+  clickedCell.innerHTML = currentPlayer;
+}
 
-squares.forEach((square) => {
+function handlePlayerChange() {
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  statusDisplay.innerHTML = currentPlayerTurn();
+}
 
-  square.addEventListener("click", (e) => {
-
-    if (currentPlayer == 'p1' && e.target.value != true) {
-
-
-      tracker = e.srcElement.className
-      markPlay(tracker, currentPlayer)
-      e.target.style.backgroundColor = 'red';
-      e.target.innerText = 'X';
-      checkWinner(playTracker, currentPlayer)
-
-      currentPlayer = 'p2';
-      turn.innerText = `Player: ${currentPlayer}`
-      e.target.value = true;
-
-
-
-    } else if (currentPlayer == 'p2' && e.target.value != true) {
-
-      tracker = e.srcElement.className
-      markPlay(tracker, currentPlayer)
-      e.target.style.backgroundColor = 'blue';
-      e.target.innerText = 'O';
-      checkWinner(playTracker, currentPlayer)
-      currentPlayer = 'p1';
-      turn.innerText = `Player: ${currentPlayer}`
-      e.target.value = true;
+function handleResultValidation() {
+  let roundWon = false;
+  for (let i = 0; i < winningConditions.length; i++) {
+    const winCondition = winningConditions[i];
+    let a = gameState[winCondition[0]];
+    let b = gameState[winCondition[1]];
+    let c = gameState[winCondition[2]]; 
+    console.log(a, b, c);
+    if (a === "" || b === "" || c === "") {
+      continue;
     }
-
-
-  })
-})
-
-
-reset.addEventListener('click', () => {
-  squares.forEach((square) => {
-    square.style.backgroundColor = 'white';
-    square.innerText = null
-    square.value = null;
-
-  })
-  currentPlayer = players[(Math.floor(Math.random() * players.length))]
-  turn.innerText = `Player: ${currentPlayer}`
-  playTracker = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-  ]
-})
-
-function markPlay(tracker, currentPlayer) {
-
-  if (tracker.includes('b0')) {
-    playTracker[0][0] = currentPlayer
-
-  } else if (tracker.includes('b1')) {
-    playTracker[0][1] = currentPlayer
-
-  } else if (tracker.includes('b2')) {
-    playTracker[0][2] = currentPlayer
-
-  } else if (tracker.includes('b3')) {
-    playTracker[1][0] = currentPlayer
-
-  } else if (tracker.includes('b4')) {
-    playTracker[1][1] = currentPlayer
-
-  } else if (tracker.includes('b5')) {
-    playTracker[1][2] = currentPlayer
-
-  } else if (tracker.includes('b6')) {
-    playTracker[2][0] = currentPlayer
-
-  } else if (tracker.includes('b7')) {
-    playTracker[2][1] = currentPlayer
-
-  } else if (tracker.includes('b8')) {
-    playTracker[2][2] = currentPlayer
-
+    if (a === b && b === c) {
+      roundWon = true;
+      break;
+    }
   }
 
-}
-
-function checkWinner(array, currentPlayer) {
-
-  if (array[0][0] && array[0][1] && array[0][2] === currentPlayer) {
-    setTimeout(() => alert(`${currentPlayer} Wins`), 1000)
-    // } else if (array[1][0] && array[1][1] && array[1][2] === 'p1') {
-    //   setTimeout(() => alert(`${currentPlayer} Wins`), 1000)
-    // } else if (array[2][0] && array[2][1] && array[2][2] === currentPlayer) {
-    //   setTimeout(() => alert(`${currentPlayer} Wins`), 1000)
-    // } else if (array[0][0] && array[1][0] && array[2][0] === currentPlayer) {
-    //   setTimeout(() => alert(`${currentPlayer} Wins`), 1000)
-    // } else if (array[2][0] && array[2][1] && array[2][2] === currentPlayer) {
-    //   setTimeout(() => alert(`${currentPlayer} Wins`), 1000)
+  if (roundWon) {
+    statusDisplay.innerHTML = winningMessage();
+    gameActive = false;
+    return;
   }
 
+  let roundDraw = !gameState.includes("");
+  if (roundDraw) {
+    statusDisplay.innerHTML = drawMessage();
+    gameActive = false;
+    return;
+  }
 
-
+  handlePlayerChange();
 }
 
+function handleCellClick(event) {
+  const clickedCell = event.target;
+  const clickedCellIndex = parseInt(clickedCell.dataset.index);
 
-// Horizontally=> 0,1,2; 3,4,5; 6,7,8
+  if (gameState[clickedCellIndex] !== "" || !gameActive) {
+    return;
+  }
 
-// Vertically => 0,3,6; 1,4,7; 2,5,8
+  handleCellPlayed(clickedCell, clickedCellIndex);
+  handleResultValidation();
+}
 
-// Diagonally=>0,4,8; 2,4,6
+function handleRestartGame() {
+  gameActive = true;
+  currentPlayer = "X";
+  gameState = ["", "", "", "", "", "", "", "", ""];
+  statusDisplay.innerHTML = currentPlayerTurn();
+  document.querySelectorAll(".cell").forEach((cell) => (cell.innerHTML = ""));
+}
+
+document
+  .querySelectorAll(".cell")
+  .forEach((cell) => cell.addEventListener("click", handleCellClick));
+document
+  .querySelector(".game--restart")
+  .addEventListener("click", handleRestartGame);
+Footer
